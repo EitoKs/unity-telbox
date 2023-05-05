57f40937907f7579441d3364e7143d77a08a0f60
EitoKs <129014371+EitoKs@users.noreply.github.com>
2023-05-05 13:12:25 +0900

    Add files via upload

--- pause/Pause.cs
+++ pause/Pause.cs
@@ -15,8 +15,16 @@ public class Pause : MonoBehaviour
 	//タイトルに戻るボタンを入れる
 	[SerializeField]
     private GameObject ExitButton;
+    //タイトルに戻るパネルを入れる
+	[SerializeField]
+    private GameObject ExitPanel;
     //メニュー画面が表示されているかどうかを判定する変数
     public bool menu_triger = false;
+
+    void Start(){
+        //タイトルに戻るパネルを非表示
+        ExitPanel.SetActive(false);
+    }
 
     //再開ボタンを押したとき発動する関数
     public void RestartMove()
@@ -27,7 +35,7 @@ public class Pause : MonoBehaviour
         PauseScript.Pause_instance.ExitPause();
     }
 
-    //終了ボタンを押したとき発動する関数（タイトル画面に戻る）
+    //タイトルに戻るで「はい」を押したとき発動する関数（タイトル画面に戻る）
     public void ExitMove()
     {
         //デバッグ用
@@ -36,5 +44,15 @@ public class Pause : MonoBehaviour
         SceneManager.LoadScene("Title");
         //時間の流れを再開する
 		Time.timeScale = 1f;
+    }
+
+    //終了ボタンを押したとき実行
+    public void ExitPanelOpen(){
+        ExitPanel.SetActive(true);      //タイトルに戻るパネルを表示
+    }
+
+    //タイトルに戻るとき「いいえ」を押したとき実行
+    public void ExitPanelClose(){
+        ExitPanel.SetActive(false);      //タイトルに戻るパネルを表示
     }
 }
