be092c67dea17d8004cc7d586691eb783c5db0da
EitoKs <129014371+EitoKs@users.noreply.github.com>
2023-05-05 13:08:02 +0900

    Add files via upload

--- inventory/InventScript.cs
+++ inventory/InventScript.cs
@@ -19,35 +19,16 @@ public class InventScript : MonoBehaviour
 	//　アイテムの画像のインスタンス
 	private GameObject InventItemInstance;
 
-	//[SerializeField]
-	//　装備画面を表示するUIのプレハブ
-	//private GameObject EquipUIPrefab;
-	//　装備画面UIのインスタンス
-	//public static GameObject EquipUIInstance;
-	//装備アイテムを表示するオブジェクト
-	//private GameObject Itembox;
-
-    //装備画面に映るアイテムの3D画像のプレハブ
-	//[SerializeField]
-	//private GameObject EquipItemPrefab;
-	//　アイテムの画像のインスタンス
-	//public static GameObject EquipItemInstance;
-
     //インベントリ画面が表示されているかどうかを判定する変数
     public static bool invent_triger = false;
 
 	//装備をしているかどうかを判定する関数
 	private bool Equip_Judge;
-	//装備画面を開いているかどうかを判定
-	//public static bool equip_triger = false;
 
-
-
-    // Update is called once per frame
     void Update()
     {
 		//ポーズ画面が開かれていないかつイベント中ではない時アイテム画面を開ける
-		if(PauseScript.MenuJudg() == false && NowEventScript.EventJudge() == false){
+		if(PauseScript.MenuJudg() == false && NowEventScript.EventJudge() == false && Continue.ContinueJudge() == false && SumahoScript.SumahoJudg() == false && NovelScript.NovelJudg() == false){
         	//Tabキーを押すとポーズ画面を開く
 			if (Input.GetKeyDown ("tab")) {
             	//プレハブがインスタンス化されてなければインスタンス化する
@@ -65,21 +46,20 @@ public class InventScript : MonoBehaviour
 					Time.timeScale = 0f;
 				} else if(invent_triger == true){ //すでにUIがインスタンス化されていればインスタンスを削除
 					//インスタンスを削除する直前のデータを確保する
-					EquipManager.EquipData();
+					NewEquipManager.instance.EquipData();
 					//インスタンスを削除
                 	Destroy (InventUIInstance);
 					//インスタンスを削除
                 	Destroy (InventItemInstance);
 
-
-					//装備をしているかどうかを判定
-					Equip_Judge = EquipManager.EquipJudge();
+					//何かしら装備をしているかどうかを判定
+					Equip_Judge = NewEquipManager.instance.EquipJudge;
 					//装備画面を開くかどうかを判定する
 					//何かしら装備をしているか
 					if(Equip_Judge == true){
-						NewEquipManager.EquipOpen();	//装備画面を開く
+						NewEquipManager.instance.EquipOpen();	//装備画面を開く
 					}else{
-						NewEquipManager.EquipClose();	//装備画面を閉じる
+						NewEquipManager.instance.EquipClose();	//装備画面を閉じる
 					}
 
                 	item_canvas.SetActive(true);
@@ -98,35 +78,4 @@ public class InventScript : MonoBehaviour
 	public static bool InventJudg(){
 		return invent_triger;
 	}
-
-	//装備画面を表示する関数
-    // void EquipOpen(){
-    //     //装備画面を閉じているとき
-    //     //if(equip_triger == false && EquipUIInstance == null){
-	// 	if(equip_triger == false){
-    //         //装備画像に表示するオブジェクトをインスタンス化する
-	// 		//EquipItemInstance = GameObject.Instantiate (EquipItemPrefab) as GameObject;
-    //         //インスタンス化する
-	// 		//EquipUIInstance = GameObject.Instantiate (EquipUIPrefab) as GameObject;
-	// 		Itembox.SetActive(true);
-    //         //装備画面を開いている判定にする
-    //         equip_triger = true;
-    //     }
-    // }
-
-    //装備画面を消す関数
-    // public static void EquipClose(){
-    //     //インスタンスを削除
-    //     //Destroy (EquipUIInstance);
-	// 	//インスタンスを削除
-    //     //Destroy (EquipItemInstance);
-	// 	//Itembox.SetActive(false);
-    //     //装備画面を開いている判定にする
-    //     equip_triger = false;
-    // }
-
-	//装備画面を開いているかどうかを返す関数
-    // public static bool EquipTriger(){
-    //     return equip_triger;
-    // }
 }
